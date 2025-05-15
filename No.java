644a9a0d8fd0350090501e3a5a3dec195503a00d
Felipe Martins <161171083+felipemartins00@users.noreply.github.com>
2025-05-15 20:23:51 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class No {
+    int valor;
+    No esquerdo;
+    No direito;
+
+    No(int valor) {
+        this.valor = valor;
+        this.direito = null;
+        this.esquerdo = null;
+
+    }
+}
